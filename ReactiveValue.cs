82b62f2c294631ec64fecd7798ea3a9d25582fb6
Doug Calderon <dmc421@gmail.com>
2018-02-13 21:26:58 -0800

    Cleaned up ReactiveValue, added IObservable support

--- ReactiveValue.cs
+++ ReactiveValue.cs
@@ -4,72 +4,77 @@ using System.Collections.Generic;
 
 namespace Unitilities
 {
-    public class ReactiveValue<T> 
+    public class ReactiveValue<T> : IObservable<T>
     {
+        public delegate void Callback(T value);
+        
+        private Callback _onChanged;
         private T _value;
-        private SmartEvent<T> _onChanged;
-        public SmartEvent<T> OnChanged => _onChanged ?? (_onChanged = new SmartEvent<T>());
-        public bool Dirty { get; private set; }
-        public static readonly EqualityComparer<T> Comparer = EqualityComparer<T>.Default;
-
         public T Value
         {
             get { return _value; }
             set
             {
-                if (Comparer.Equals(_value, value)) 
+                if (EqualityComparer<T>.Default.Equals(_value, value)) 
                     return;
-                Dirty = true;
                 _value = value;
                 _onChanged?.Invoke(value);
             }
         }
+        
+        public ReactiveValue(T value)
+        {
+            _value = value;
+        }
 
         public void Set(T value) => Value = value;
 
-        public void Subscribe(Action<T> callback, bool immediate = true)
+        public void Subscribe(Callback callback, bool immediate = true)
         {
-            OnChanged.Add(callback);
+            _onChanged += callback;
             if (immediate)
                 callback(Value);
         }
 
         public void Select(Predicate<T> filter, Action<T> callback, bool immediate = true)
         {
-            OnChanged.Add(obj => {
-                if (filter(obj))
-                    callback(obj);
-            });
+            _onChanged += val => {
+                if (filter(val))
+                    callback(val);
+            };
             if (immediate && filter(Value))
                 callback(Value);
         }
 
-        public void Clean()
-        {
-            Dirty = false;
-        }
-
-        public ReactiveValue(T value, bool dirty = false)
-        {
-            _value = value;
-            Dirty = dirty;
-        }
-
+        public void Unsubscribe(Callback callback) => _onChanged -= callback;
+        
         public static implicit operator T(ReactiveValue<T> rv) => rv.Value;
 
-        public override int GetHashCode()
+        public override int GetHashCode() => Value.GetHashCode();
+
+        public override bool Equals(object obj) => Value.Equals(obj);
+        
+        public override string ToString() => Value.ToString();
+
+        public class Ticket : IDisposable
         {
-            return Value.GetHashCode();
+            private readonly ReactiveValue<T> _rv;
+            private readonly Callback _cb;
+
+            public Ticket(ReactiveValue<T> rv, Callback cb)
+            {
+                _rv = rv;
+                _cb = cb;
+            }
+
+            public void Dispose() => _rv.Unsubscribe(_cb);
         }
 
-        public override bool Equals(object obj)
+        public IDisposable Subscribe(IObserver<T> observer)
         {
-            return Value.Equals(obj);
-        }
-
-        public override string ToString()
-        {
-            return Value.ToString();
+            Callback cb = observer.OnNext;
+            _onChanged += cb;
+            return new Ticket(this, cb);
         }
     }
 }
